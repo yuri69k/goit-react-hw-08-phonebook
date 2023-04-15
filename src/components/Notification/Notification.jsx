06008e8bf7633ecc39732655167bf63e335c85bd
yuri.karmaz <106161829+yuri69k@users.noreply.github.com>
2023-04-15 21:59:08 +0300

    material Notification

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,7 +1,11 @@
 import PropTypes from 'prop-types';
-import css from './Notification.module.css';
+import Typography from '@mui/material/Typography';
 
-export const Notification = ({ message }) => <p className={css.message}>{message}</p>;
+export const Notification = ({ message }) => (
+  <Typography variant="h6" style={{ textAlign: 'center' }}>
+    {message}
+  </Typography>
+);
 
 Notification.propTypes = {
   message: PropTypes.string,
