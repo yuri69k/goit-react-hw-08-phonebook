bd61ab92da10288b2ea011a3fe794889047de3d8
yuri.karmaz <106161829+yuri69k@users.noreply.github.com>
2023-04-15 21:53:17 +0300

    fix Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,19 +1,53 @@
 import { NavLink } from 'react-router-dom';
-// import { useAuth } from 'hooks';
-import styles from './Navigation.module.scss';
+import { useAuth } from 'hooks';
+import Link from '@mui/material/Link';
+import IconButton from '@mui/material/IconButton';
+import CottageRoundedIcon from '@mui/icons-material/CottageRounded';
+import ContactPhoneRoundedIcon from '@mui/icons-material/ContactPhoneRounded';
+import Tooltip from '@mui/material/Tooltip';
+
+const styles = {
+  link: {
+    marginLeft: '15px',
+    position: 'relative',
+
+    '&.active::after': {
+      content: '""',
+      display: 'block',
+      height: '40px',
+      width: '40px',
+      borderRadius: '50%',
+      backgroundColor: '#be4d25',
+      position: 'absolute',
+      bottom: '0',
+      left: '0',
+      boxShadow: '0 0 10px 1px #be4d25',
+      opacity: 0.2,
+    },
+  },
+};
 
 export const Navigation = () => {
-  // const { isLoggedIn } = useAuth();
+  const { isLoggedIn } = useAuth();
 
   return (
     <nav>
-      <NavLink className={styles.link} to="/">
-        Home
-      </NavLink>
-
-      <NavLink className={styles.link} to="/contacts">
-        Contacts
-      </NavLink>
+      <Link component={NavLink} to="/" sx={styles.link}>
+        <Tooltip title="Home" placement="bottom" arrow>
+          <IconButton>
+            <CottageRoundedIcon />
+          </IconButton>
+        </Tooltip>
+      </Link>
+      {isLoggedIn && (
+        <Link component={NavLink} to="/contacts" sx={styles.link}>
+          <Tooltip title="Contacts" placement="bottom" arrow>
+            <IconButton>
+              <ContactPhoneRoundedIcon />
+            </IconButton>
+          </Tooltip>
+        </Link>
+      )}
     </nav>
   );
 };
