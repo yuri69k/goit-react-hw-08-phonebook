956dbfcc77557a9c5e83aea88d375e13b187ba90
yuri.karmaz <106161829+yuri69k@users.noreply.github.com>
2023-04-15 22:21:56 +0300

    add CircularProgress in Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,28 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import { AppBar } from '../AppBar/AppBar';
-
+import CircularProgress from '@mui/material/CircularProgress';
 import Container from '@mui/material/Container';
-import { Notification } from 'components/Notification/Notification';
+
 
 
 export const Layout = () => (
   <Container maxWidth="md">
     <AppBar />
-    <Suspense fallback={<Notification message="Loading..." />}>
+    <Suspense    fallback={
+        <div
+          style={{
+            width: '100%',
+            height: 'calc(100vh - 64px)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            position: 'fixed',
+          }}
+        >
+          <CircularProgress />
+        </div>
+      }>
       <Outlet />
     </Suspense>
  </Container>
